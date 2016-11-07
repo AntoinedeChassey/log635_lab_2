dd25c6e48a6be3ff0262232c6d62a2b6181554ec
Antoine de Chassey <antoinedechassey@gmail.com>
2016-11-07 11:13:27 -0600

    Fr

--- ParcoursSemantiqueEnNLTK3/main.py
+++ ParcoursSemantiqueEnNLTK3/main.py
@@ -13,9 +13,9 @@ import nltk
 caps = "([A-Z])"
 prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
 suffixes = "(Inc|Ltd|Jr|Sr|Co)"
-starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+starters = "(Mr|Mrs|Ms|Dr|Il\s|Elle\s|Ce\s|Ils\s|Leur\s|Notre\s|Nous\s|Mais\s|However\s|That\s|This\s|Wherever)"
 acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
-websites = "[.](com|net|org|io|gov)"
+websites = "[.](com|fr|net|org|io|gov)"
 
 class nltkTextParser:
     def __init__(self, textFile, grammarFile):
